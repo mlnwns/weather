015ccd4f53d81b289a11fc8b46f884df543c75e7
mlnwns <mlnwns@naver.com>
2026-04-11 00:55:38 +0900

    chore: ESLint 및 Prettier 설정

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,5 @@
 function App() {
-  return (
-    <h1 className="text-3xl font-bold text-blue-600">Tailwind CSS 적용 완료</h1>
-  );
+  return <h1 className="text-3xl font-bold text-blue-600">Tailwind CSS 적용 완료</h1>;
 }
 
 export default App;
