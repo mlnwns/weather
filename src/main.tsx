9f78555882d9b4ac344c90510008edc148c9d92e
mlnwns <mlnwns@naver.com>
2026-04-11 01:27:14 +0900

    feat: AppProviders 컴포넌트 분리

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import '@app/styles/global.css';
-import { App } from '@/app';
-import { QueryClientProvider } from '@tanstack/react-query';
-import { queryClient } from '@app/providers/query-client';
+import { App, AppProviders } from '@app';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <AppProviders>
       <App />
-    </QueryClientProvider>
+    </AppProviders>
   </StrictMode>,
 );
