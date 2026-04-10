4b2c82741bf1142deaee68cc60b10ec280d578a8
mlnwns <mlnwns@naver.com>
2026-04-11 01:10:48 +0900

    feat: TanStack Query 패키지 설치 및 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "@app/styles/global.css";
-import { App } from "@/app";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import '@app/styles/global.css';
+import { App } from '@/app';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from '@app/providers/query-client';
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 );
